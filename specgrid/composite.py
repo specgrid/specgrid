b3b7e4bb30163a95c5eeeea70e1c63f8092b8bb2
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-03-27 15:42:37 -0400

    fixed

--- specgrid/composite.py
+++ specgrid/composite.py
@@ -8,14 +8,14 @@ class CompositeModel(object):
         for model in models_list:
             self.param2model.update(dict([(param, model) for param in model.parameters]))
 
-    def __getattr__(self, item):
+    def __getattribute__(self, item):
         if item in self.param2model:
             return getattr(self.param2model[item], item)
         else:
-            return super(CompositeModel, self).__getattr__(item)
+            return super(CompositeModel, self).__getattribute__(item)
 
-    def __setattr__(self, item, value):
+    def __setattribute__(self, item, value):
         if item in self.param2model:
             return setattr(self.param2model[item], item, value)
         else:
-            return super(CompositeModel, self).__setattr__(item, value)
+            return super(CompositeModel, self).__setattribute__(item, value)
