1961363dc1f37db5587c45d7986e4a1e7cfb8e94
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2014-03-27 15:56:09 -0400

    Composite attribute access fixed

--- specgrid/composite.py
+++ specgrid/composite.py
@@ -2,20 +2,23 @@
 
 class CompositeModel(object):
 
+    param2model = {}
+
     def __init__(self, models_list):
         self.models_list = models_list
         self.param2model = {}
         for model in models_list:
-            self.param2model.update(dict([(param, model) for param in model.parameters]))
+            self.param2model.update(dict([(param, model)
+                                          for param in model.parameters]))
 
     def __getattr__(self, item):
         if item in self.param2model:
             return getattr(self.param2model[item], item)
         else:
-            raise AttributeError
+            return super(CompositeModel, self).__getattribute__(item)
 
     def __setattr__(self, item, value):
         if item in self.param2model:
             return setattr(self.param2model[item], item, value)
         else:
-            raise AttributeError
+            super(CompositeModel, self).__setattr__(item, value)
