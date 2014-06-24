a6f095cbf8ca7f302913d947d4baf4e6030cebde
Nathan Winsor <nwwinsor@hotmail.ca>
2014-06-24 17:38:52 -0400

    Cleaned up the test_plugins.

--- specgrid/tests/test_plugins.py
+++ specgrid/tests/test_plugins.py
@@ -5,13 +5,6 @@ from astropy import units as u
 from specgrid.plugins import Convolve, Interpolate
 from scipy.integrate import simps
 
-"""
-The two lines of code below are for the optional plotting feature (see further for more details). Remove the ''' above and below the two lines of code to turn on this feature of the test
-"""
-
-'''
-import matplotlib.pyplot as plt
-'''
 
 def test_convolution():
     my_spec = Spectrum1D.from_array(np.linspace(6000, 8000, 2000), np.ones(2000), dispersion_unit = 'Angstrom', unit = ' erg / (cm^2 s Angstrom)')
@@ -27,27 +20,8 @@ def test_convolution():
 
     integral_pre_convolved = simps(my_spec.flux, my_spec.wavelength.value)
     integral_convolved = simps(conv_spectrum.flux, conv_spectrum.wavelength.value)
-    #This allows you to manually check the values of the integrals. They should be very close in value if the gaussian convolution was sucessful. 
-    print "integral_pre_convolved = {0}".format(integral_pre_convolved)
-    print "integral_convolved = {0}".format(integral_convolved)
-    #When I tested this, I got the values: integral_pre_convolved = 1998.99949975 and integral_convolved = 1998.99949975.
 
     npt.assert_allclose(integral_pre_convolved, integral_convolved, rtol = 1.0, atol=0.0002)
-
-
-    """
-    Optional plotting, to get a feel for why this test works conceptually. To use this feature, remove the ''' above and below the code, and zoom into the plot at x = 7000 to see the desired effect.
-    """
-    '''
-    plt.clf()
-    plt.figure(1)
-    plt.xlim(5500, 8500)
-    plt.ylim(-0.25, 1.25)
-    plt.plot(my_spec.wavelength.value, my_spec.flux, color= 'blue', label = 'my_spec')
-    plt.plot(conv_spectrum.wavelength.value, conv_spectrum.flux, color = 'red', label = 'conv_spectrum')
-    plt.legend(loc = 'upper right')
-    plt.show()
-    '''
 
 
 def test_interpolate():
@@ -59,27 +33,6 @@ def test_interpolate():
 
     expected_interpolated_flux = np.arange(1,26)
     interpolated_test_flux = interpolated_test_spectrum.flux
-    
-    #This allows you to manually check the mean value of the interpolated test flux. This should be an array of 1 to 25 (spacing of 1).
-    print "interpolated_test_flux = {0}".format(interpolated_test_flux)
-    #When I tested this, I got: interpolated_test_flux = [  1.   2.   3.   4.   5.   6.   7.   8.   9.  10.  11.  12.  13.  14.  15.  16.  17.  18.  19.  20.  21.  22.  23.  24.  25.] ... As expected.
 
     npt.assert_array_almost_equal(interpolated_test_flux, expected_interpolated_flux, decimal = 6)
 
-    """
-    Optional plotting, to get a feel for why this test works conceptually. To use this feature, remove the ''' below, and take notice that the green and red markers lie on the same x-values, while the green and blue markers DO NOT lie on the same x-values (given that the interpolate worked correctly).
-    """
-    '''
-    plt.clf()
-    plt.figure(1)
-    plt.xlim(0, 26)
-    plt.ylim(-1, 26)
-    plt.plot(obs_spectrum.wavelength.value, obs_spectrum.flux, marker = 'o', linestyle = 'None', color = 'green', label = 'obs_spectrum')
-    plt.plot(test_spectrum.wavelength.value, test_spectrum.flux, marker = 'o', linestyle = 'None', color = 'blue', label = 'test_spectrum')
-    plt.plot(interpolated_test_spectrum.wavelength.value, interpolated_test_spectrum.flux, marker = 'o', linestyle = 'None', color = 'red', label = 'interpolated_test_spectrum')
-    plt.legend(loc = 'upper left', numpoints = 1)
-    plt.grid()
-    plt.xticks([i for i in range(0,27)])
-    plt.yticks([i for i in range(0,27)])
-    plt.show()
-    '''
