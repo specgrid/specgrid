0ad3f45299c37406fcdeff76c70bf8766d3c3b8a
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-10-14 18:08:24 -0400

    fixed typo

--- specgrid/setup_package.py
+++ specgrid/setup_package.py
@@ -1,4 +1,4 @@
 def get_package_data():
     return {
-        _ASTROPY_PACKAGE_NAME: ['data/munari_small.h5', 'data/test_data.h5',
+        _ASTROPY_PACKAGE_NAME_: ['data/munari_small.h5', 'data/test_data.h5',
                                 'data/test_spec.txt']}
